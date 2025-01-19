0e38d793ec55ee2f4fdb465d75a2cbfb9b9c4af0
Nebulans19 <155171827+Nebulans19@users.noreply.github.com>
2025-01-20 01:07:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-document.querySelectorAll('nav ul li a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+document.addEventListener('DOMContentLoaded', function() {
+    const tocLinks = document.querySelectorAll('.table-of-contents a');
+    
+    tocLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = link.getAttribute('href').substring(1);
+            document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
         });
     });
 });
